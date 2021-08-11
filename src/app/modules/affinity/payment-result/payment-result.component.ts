40d68c767cf5e198c825de97f9553e5f9aaee9af
Jethru Balarbar <marx.balarbar@tooltwist.com>
2021-08-11 20:55:57 +0800

    Update payment-result.component.ts
    test

--- src/app/modules/affinity/payment-result/payment-result.component.ts
+++ src/app/modules/affinity/payment-result/payment-result.component.ts
@@ -84,6 +84,8 @@ export class PaymentResultComponent implements OnInit {
     this.caller.doCallService('/afnty/retrievePolicyDetails', policyNumber).subscribe(
       result => {
         this.spinner.hide();
+        this.affinity = result;
+
         this.affinity.paymentReferenceNumber = result.a2990700_mph.numPaymentReference;
 
         switch (result.p2000030.codRamo) {
