515fed70560247dfcb4bd4c3dd26dc73ee851cc6
marxbal <marxbal@mapfreinsurance.com.ph>
2022-02-16 14:17:54 +0800

    added gcash payment

--- src/app/modules/affinity/risk-motor/risk-motor.component.ts
+++ src/app/modules/affinity/risk-motor/risk-motor.component.ts
@@ -368,6 +368,9 @@ export class RiskMotorComponent implements OnInit {
     if (this.affinity.paymentOption == "cc") {
       option = "Credit Card as your payment option";
       imges = "<img src='assets/images/paynamics-logo.png' style='width: 100px; margin: 0 auto;' alt='paynamics-logo'/>";
+    } else if (this.affinity.paymentOption == "gc") {
+      option = "GCash as your payment option";
+      imges = "<img src='assets/images/paynamics-logo.png' style='width: 100px; margin: 0 auto;' alt='paynamics-logo'/>";
     }
 
     Swal.fire({
