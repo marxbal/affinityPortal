800be581ea9e2e4af6dfaf3249006f75767d0928
marxbal <marxbal@mapfreinsurance.com.ph>
2022-02-23 10:19:09 +0800

    displaying chat email

--- src/app/modules/affinity/footer/footer.component.ts
+++ src/app/modules/affinity/footer/footer.component.ts
@@ -21,15 +21,20 @@ export class FooterComponent implements OnInit {
     private auth: AuthenticationService) {}
 
   partnerName: string = "";
+  chatEmail: string = "";
   brokenUrl: boolean = false;
+  hasPartner: boolean = true;
 
   ngOnInit() {
     const partner = this.auth.getPartner() as Partner;
     if (!_.isEmpty(partner)) {
       this.partnerName = _.toLower(partner.partnerName);
+      this.chatEmail = partner.chatEmail;
       if (this.partnerName == 'sitel') {
         this.partnerName = 'marsh';
       }
+    } else {
+      this.hasPartner = false;
     }
   }
 
